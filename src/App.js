83fe26f5f0de3fa026bcc970002c95e233856a4c
Oriol Carbo <oriol.carbo@gmail.com>
2019-06-25 16:55:38 +0200

    ADDS upload form for image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
+import { Upload, message, Button, Icon } from 'antd';
+import 'antd/dist/antd.css';
+
 import logo from './logo.svg';
 import './App.css';
 
+
+const props = {
+  name: 'file',
+  listType: 'picture',
+  action: 'https://www.mocky.io/v2/5185415ba171ea3a00704eed',
+  headers: {
+    authorization: 'authorization-text',
+  },
+  onChange(info) {
+    if (info.file.status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
+
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Upload {...props}>
+      <Button>
+        <Icon type="upload" /> Click to Upload
+      </Button>
+    </Upload>
   );
 }
 
